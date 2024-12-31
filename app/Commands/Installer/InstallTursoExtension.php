a37fc45361fb26edd63ee3cd6435d88b4b2cfe24
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-01 05:53:13 +0700

    refactor(installer): streamline installation flow with nonInteractive and askQuestions methods

--- app/Commands/Installer/InstallTursoExtension.php
+++ app/Commands/Installer/InstallTursoExtension.php
@@ -3,23 +3,28 @@
 namespace App\Commands\Installer;
 
 use App\Contracts\Installer;
+use App\Traits\HasInstallQuestions;
+use App\Traits\NonInteractive;
 use LaravelZero\Framework\Commands\Command;
 
+use function Laravel\Prompts\confirm;
 use function Laravel\Prompts\info;
 
 class InstallTursoExtension extends Command
 {
+    use NonInteractive, HasInstallQuestions;
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'install 
-        {--unstable : Install the unstable version} 
+        {--unstable : Install the unstable version from development repository} 
+        {--thread-safe : Install the Thread Safe (TS) version}
         {--php-ini= : Specify the php.ini file}
         {--php-version= : Specify the PHP version}
         {--extension-dir= : Specify the PHP extension directory}
-        {--non-thread-safe : Install the non-thread-safe version}';
+    ';
 
     /**
      * The console command description.
@@ -34,32 +39,18 @@ class InstallTursoExtension extends Command
     public function handle(Installer $installer)
     {
         if ($installer->checkIfAlreadyInstalled()) {
-            info(" Turso libSQL Extension for PHP is already installed. Skipping installation... \n Use the `update` command to update the extension.");
-            return;
+            if (!confirm(" Turso libSQL Extension for PHP is already installed. Update the extension?", false)) {
+                info(" Skipping installation... \n Use the `update` command to update the extension.");
+                return;
+            }
         }
 
-        if ($this->option('php-ini')) {
-            $installer->setPhpIni($this->option('php-ini'));
-        }
-
-        if ($this->option('php-version')) {
-            $installer->setPhpVersion($this->option('php-version'));
-        }
-
-        if ($this->option('extension-dir')) {
-            $installer->setExtensionDir($this->option('extension-dir'));
-        }
-
-        if ($this->option('non-thread-safe')) {
-            $installer->setNonThreadSafe();
-        }
-
-        if ($this->option('unstable')) {
-            $installer->setUnstable(true);
-        }
+        $this->nonInteractive($installer);
 
         info('Installing libSQL Extension for PHP...');
-        $installer->install();
+        
+        $this->askQuestions($installer);
+        
         info('  ✨ libSQL Extension for PHP installed');
     }
 }
