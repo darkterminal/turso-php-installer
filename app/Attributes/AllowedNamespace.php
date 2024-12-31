00cbf826d55fd76c5c5f4c35e0644a3812d34942
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-01 05:01:54 +0700

    feat(attributes): introduce PHP attribute for restricting class usage to specific namespaces

---
+++ app/Attributes/AllowedNamespace.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class AllowedNamespace
+{
+
+    public function __construct(public array|string $namespaces)
+    {
+        if (is_string($namespaces)) {
+            $this->namespaces = [$namespaces];
+        }
+    }
+
+    /**
+     * Validate if the given class belongs to one of the allowed namespaces.
+     *
+     * @param string $class The fully qualified class name
+     * @return bool
+     */
+    public function isClassAllowed(string $class): bool
+    {
+        foreach ($this->namespaces as $namespace) {
+            if ($class === $namespace) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
