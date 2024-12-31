18ff0922b6cefa2c93bca77b39ca80b213a5a9f8
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-01 05:55:16 +0700

    refactor(database): update get_all_database_token query to return selected fields

--- app/database/get_all_database_token.sql
+++ app/database/get_all_database_token.sql
@@ -1,1 +1,14 @@
-SELECT * FROM tokens ORDER BY created_at DESC
+SELECT
+    id,
+    db_name,
+    full_access_token,
+    read_only_token,
+    public_key_pem,
+    public_key_base64,
+    expiration_day,
+    created_at,
+    updated_at
+FROM
+    tokens
+ORDER BY
+    created_at DESC
