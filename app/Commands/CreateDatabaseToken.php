2f76a719e4658f0e499a15c31f1025ad53e72147
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-12-31 10:23:41 +0700

    chore(token): check the extension before generating token

--- app/Commands/CreateDatabaseToken.php
+++ app/Commands/CreateDatabaseToken.php
@@ -2,8 +2,8 @@
 
 namespace App\Commands;
 
+use App\Contracts\Installer;
 use App\Repositories\DatabaseTokenGenerator;
-use App\Repositories\Installer;
 use LaravelZero\Framework\Commands\Command;
 
 class CreateDatabaseToken extends Command
@@ -25,12 +25,13 @@ class CreateDatabaseToken extends Command
     /**
      * Execute the console command.
      */
-    public function handle(DatabaseTokenGenerator $databaseTokenGenerator)
+    public function handle(DatabaseTokenGenerator $databaseTokenGenerator, Installer $installer)
     {
-        if (!(new Installer())->checkIsAlreadyExists()) {
+        if (!$installer->checkIfAlreadyInstalled()) {
             $this->error("Turso libSQL Extension for PHP is not installed. Please install it first.");
-            exit;
+            return;
         }
+
         $this->comment("Creating libSQL Server Database token for Local Development...");
         $dbName = $this->argument('db-name');
         $this->info((new DatabaseTokenGenerator())->generete($dbName)->toJSON(true));
