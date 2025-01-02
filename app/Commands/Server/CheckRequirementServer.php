5cbb527ac866a352c61b9a6283977e6fe2fb4cc6
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-03 05:12:13 +0700

    feat(server): add command to check server requirements

---
+++ app/Commands/Server/CheckRequirementServer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Commands\Server;
+
+use App\Contracts\ServerGenerator;
+use LaravelZero\Framework\Commands\Command;
+
+class CheckRequirementServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:check';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check server requirement, this will check if python3 pip and cyptography lib are installed';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ServerGenerator $server)
+    {
+        $this->info("  Checking server requirement...");
+        if ($server->checkRequirement()) {
+            $this->info('  ✨ Server requirement met');
+        }
+    }
+}
