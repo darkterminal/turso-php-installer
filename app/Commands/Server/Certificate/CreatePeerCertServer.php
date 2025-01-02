736ee6c2faaf0034499857f0550d245b3fb1300e
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-03 05:13:06 +0700

    feat(server): add command to generate peer certificates

---
+++ app/Commands/Server/Certificate/CreatePeerCertServer.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Commands\Server\Certificate;
+
+use App\Contracts\ServerGenerator;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class CreatePeerCertServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:ca-peer-cert-create {name=ca}
+        {--expiry=30 : Expiry in days, default is 30 days}
+    ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a peer certificate';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ServerGenerator $server)
+    {
+        if (!$server->checkRequirement()) {
+            $this->error(' ❗ The required Python packages are not installed. Please install them and try again.');
+            return;
+        }
+
+        if ($server->createPeerCert($this->argument('name'), $this->option('expiry'))) {
+            $this->info('  ✨ Peer certificate created and store at ' . get_global_metadata('cert_store_location'));
+            return;
+        }
+        $this->error(" 🙈 Peer certificate creation failed");
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
