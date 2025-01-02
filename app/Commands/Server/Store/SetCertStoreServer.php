b65706dcf440220c464eec5645a4e4ffbd35e28a
Imam Ali Mustofa <d.darkterminal@gmail.com>
2025-01-03 05:15:20 +0700

    feat(server): add new command to set your custom certificate store

---
+++ app/Commands/Server/Store/SetCertStoreServer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Commands\Server\Store;
+
+use App\Contracts\ServerGenerator;
+use LaravelZero\Framework\Commands\Command;
+
+class SetCertStoreServer extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'server:cert-store-set {path?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set/overwrite global certificate store, to use by the server later. Default is same as {installation_dir}/certs';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(ServerGenerator $server)
+    {
+        $this->info("  Setting cert store location...");
+        $path = $this->argument('path') ?? get_plain_installation_dir() . DS . 'certs';
+        $server->setCertStoreLocation($path);
+        $this->info('  ✨ Cert store location set');
+    }
+}
